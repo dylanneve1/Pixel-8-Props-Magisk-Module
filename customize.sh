249cd9bef662e73d3b515e5c5b14f11a6a995443
Dylan Neve <dylanneve1@gmail.com>
2023-11-16 19:09:02 +0000

    customize: remove post-install message

--- customize.sh
+++ customize.sh
@@ -37,6 +37,3 @@ fi
 for i in $(find $MODPATH -type f -name "*.sh" -o -name "*.prop" -o -name "*.rule"); do
   [ -f $i ] && { sed -i -e "/^#/d" -e "/^ *$/d" $i; [ "$(tail -1 $i)" ] && echo "" >> $i; } || continue
 done
-
-# Print footer message
-ui_print "- Now enjoy your Pixel - t.me/GappsLeaks"
